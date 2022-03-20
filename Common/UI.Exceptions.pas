c7bcdcbd847e9d1a93ecf901783e279c29c00664
diversenok <diversenok@gmail.com>
2022-03-20 04:45:32 +0100

    Add stack trace support to exception handling

--- Common/UI.Exceptions.pas
+++ Common/UI.Exceptions.pas
@@ -1,7 +1,8 @@
 unit UI.Exceptions;
 
 {
-  This module allows configuring default exception handling for UI applications.
+  This module allows configuring default exception handling for UI applications
+  and adds support for capturing stack traces.
 }
 
 interface
@@ -9,11 +10,14 @@ interface
 uses
   System.SysUtils, DelphiUtils.AutoEvents;
 
-// Show the default exception dialog
+// Show NtUiLib exception dialog
 procedure ReportException(E: Exception);
 
-// Adjust Application.OnException to use our exception dialog.
-procedure EnableNtxExceptionHandling;
+// Set NtUiLib exception dialog as the hander for Application.OnException
+procedure EnableNtUiLibExceptionHandling;
+
+// Add stack trace support to exception handling
+procedure EnableStackTracingExceptions;
 
 type
   // Custom exception-safe invokers for automatic events
@@ -37,7 +41,15 @@ type
 implementation
 
 uses
-  DelphiUtils.AutoObjects, NtUiLib.Exceptions.Dialog, Vcl.Forms;
+  NtUtils, NtUtils.Ldr, NtUtils.DbgHelp, DelphiUtils.AutoObjects,
+  NtUiLib.Exceptions.Dialog, Vcl.Forms;
+
+{ NtUiLib Exception Hanlder }
+
+procedure ReportException;
+begin
+  ShowNtxException(Application.Handle, E)
+end;
 
 type
   TUIExceptionHandler = class
@@ -45,12 +57,7 @@ type
     class var Instance: IAutoObject<TUIExceptionHandler>;
   end;
 
-procedure ReportException;
-begin
-  ShowNtxException(Application.Handle, E)
-end;
-
-procedure EnableNtxExceptionHandling;
+procedure EnableNtUiLibExceptionHandling;
 begin
   // Cache the instance since we need a "procedure of object"
   if not Assigned(TUIExceptionHandler.Instance) then
@@ -64,7 +71,70 @@ begin
   ReportException(E);
 end;
 
-{ TExceptionSafeInvoker }
+{ Stack Trace Support }
+
+// A callback for capturing the stack trace when an exception occurs
+function GetExceptionStackInfoProc(P: PExceptionRecord): Pointer;
+var
+  Trace: TArray<Pointer> absolute Result;
+  i: Integer;
+begin
+  // Clean-up before assigning
+  Result := nil;
+
+  // Capture the backtrace
+  Trace := RtlxCaptureStackTrace;
+
+  // Trim it by removing exception-handling frames
+  for i := 0 to High(Trace) do
+    if Trace[i] = P.ExceptionAddress then
+    begin
+      Delete(Trace, 0, i);
+      Break;
+    end;
+end;
+
+{$IFDEF Win64}
+// A callback for representing the stack trace
+function GetStackInfoStringProc(Info: Pointer): string;
+var
+  Trace: TArray<Pointer> absolute Info;
+  Modules: TArray<TModuleEntry>;
+  Frames: TArray<String>;
+  i: Integer;
+begin
+  Modules := LdrxEnumerateModules;
+  SetLength(Frames, Length(Trace));
+
+  for i := 0 to High(Trace) do
+    Frames[i] := SymxFindBestMatch(Modules, Trace[i]).ToString;
+
+  Result := String.Join(#$D#$A, Frames);
+end;
+{$ELSE}
+function GetStackInfoStringProc(Info: Pointer): string;
+begin
+  // TODO: fix NtUtils's DbgHelp support on WoW64
+  // TODO: fallback to export-based symbol enumeration
+  Result := '(not supported under WoW64)';
+end;
+{$ENDIF}
+
+procedure CleanUpStackInfoProc(Info: Pointer);
+var
+  Trace: TArray<Pointer> absolute Info;
+begin
+  Finalize(Trace);
+end;
+
+procedure EnableStackTracingExceptions;
+begin
+  Exception.GetExceptionStackInfoProc := GetExceptionStackInfoProc;
+  Exception.GetStackInfoStringProc := GetStackInfoStringProc;
+  Exception.CleanUpStackInfoProc := CleanUpStackInfoProc;
+end;
+
+{ Safe Event Invoker }
 
 class procedure TExceptionSafeInvoker.NoParameters;
 begin
