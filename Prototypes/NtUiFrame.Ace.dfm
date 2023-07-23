0b9aa0cd042cac798846b4df2eeb2b0625186483
diversenok <diversenok@gmail.com>
2023-07-23 16:33:39 +0200

    Add ACE editor frame

---
+++ Prototypes/NtUiFrame.Ace.dfm
@@ -0,0 +1,289 @@
+object AceFrame: TAceFrame
+  Left = 0
+  Top = 0
+  Width = 600
+  Height = 581
+  Constraints.MinHeight = 500
+  Constraints.MinWidth = 460
+  DoubleBuffered = True
+  ParentDoubleBuffered = False
+  ParentShowHint = False
+  ShowHint = True
+  TabOrder = 0
+  OnResize = SplitterMoved
+  object lblType: TLabel
+    Left = 0
+    Top = 0
+    Width = 600
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    AutoSize = False
+    Caption = 'Type:'
+  end
+  object lblFlags: TLabel
+    Left = 354
+    Top = 184
+    Width = 29
+    Height = 13
+    Anchors = [akTop, akRight]
+    Caption = 'Flags:'
+  end
+  object lblAccessMask: TLabel
+    Left = 3
+    Top = 184
+    Width = 37
+    Height = 13
+    Caption = 'Access:'
+  end
+  object lblSid: TLabel
+    Left = 0
+    Top = 46
+    Width = 600
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    AutoSize = False
+    Caption = 'SID:'
+  end
+  object lblServerSid: TLabel
+    Left = 0
+    Top = 92
+    Width = 600
+    Height = 13
+    Hint = 
+      'Compound ACEs introduce two access checks: the first (client) SI' +
+      'D is checked against the effective token; the second (server) SI' +
+      'D is always checked against the primary (process) token of the c' +
+      'aller.'
+    Anchors = [akLeft, akTop, akRight]
+    AutoSize = False
+    Caption = 'Server SID:'
+    Enabled = False
+  end
+  object lblCondition: TLabel
+    Left = 0
+    Top = 138
+    Width = 600
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    AutoSize = False
+    Caption = 'Condition:'
+    Enabled = False
+  end
+  object lblExtraData: TLabel
+    Left = 0
+    Top = 538
+    Width = 600
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    AutoSize = False
+    Caption = 'Extra Data:'
+  end
+  object cbxType: TComboBox
+    Left = 0
+    Top = 18
+    Width = 600
+    Height = 22
+    Style = csOwnerDrawFixed
+    Anchors = [akLeft, akTop, akRight]
+    DropDownCount = 22
+    ItemIndex = 0
+    TabOrder = 0
+    Text = '0: Access Allowed ACE'
+    OnChange = cbxTypeChange
+    Items.Strings = (
+      '0: Access Allowed ACE'
+      '1: Access Denied ACE'
+      '2: System Audit ACE'
+      '3: System Alarm ACE'
+      '4: Access Allowed Compound ACE'
+      '5: Access Allowed Object ACE'
+      '6: Access Denied Object ACE'
+      '7: System Audit Object ACE'
+      '8: System Alarm Object ACE'
+      '9: Access Allowed Callback ACE'
+      '10: Access Denied Callback ACE'
+      '11: Access Allowed Callback Object ACE'
+      '12: Access Denied Callback Object ACE'
+      '13: System Audit Callback ACE'
+      '14: System Alarm Callback ACE'
+      '15: System Audit Callback Object ACE'
+      '16: System Alarm Callback Object ACE'
+      '17: System Mandatory Label ACE'
+      '18: System Resource Attribute ACE'
+      '19: System Scoped Policy Id ACE'
+      '20: System Process Trust Label ACE'
+      '21: System Access Filter ACE')
+  end
+  inline fmxSid: TSidEditor
+    Left = 0
+    Top = 62
+    Width = 600
+    Height = 25
+    Anchors = [akLeft, akTop, akRight]
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 1
+  end
+  inline fmxServerSid: TSidEditor
+    Left = 0
+    Top = 108
+    Width = 600
+    Height = 25
+    Anchors = [akLeft, akTop, akRight]
+    Enabled = False
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 2
+  end
+  object cbxObjectType: TCheckBox
+    Left = 0
+    Top = 442
+    Width = 600
+    Height = 17
+    Anchors = [akLeft, akRight, akBottom]
+    Caption = 'Object Type:'
+    Enabled = False
+    TabOrder = 5
+    OnClick = cbxObjectTypeClick
+  end
+  object cbxInheritedObjectType: TCheckBox
+    Left = 0
+    Top = 490
+    Width = 600
+    Height = 17
+    Anchors = [akLeft, akRight, akBottom]
+    Caption = 'Inherited Object Type:'
+    Enabled = False
+    TabOrder = 6
+    OnClick = cbxInheritedObjectTypeClick
+  end
+  object BitsPanel: TPanel
+    Left = 0
+    Top = 202
+    Width = 600
+    Height = 235
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    BevelOuter = bvNone
+    ShowCaption = False
+    TabOrder = 4
+    object Splitter: TSplitter
+      Left = 338
+      Top = 0
+      Width = 6
+      Height = 235
+      Align = alRight
+      MinSize = 210
+      ResizeStyle = rsLine
+      OnMoved = SplitterMoved
+    end
+    inline fmxFlags: TBitsFrame
+      Left = 344
+      Top = 0
+      Width = 256
+      Height = 235
+      Align = alRight
+      Constraints.MinHeight = 100
+      Constraints.MinWidth = 200
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 1
+      inherited Tree: TDevirtualizedTree
+        Width = 256
+        Height = 207
+        Columns = <
+          item
+            Options = [coAllowClick, coDraggable, coEnabled, coParentBidiMode, coParentColor, coResizable, coVisible, coAutoSpring, coSmartResize, coAllowFocus, coDisableAnimatedResize, coEditable, coStyleColor]
+            Position = 0
+            Text = 'Name'
+            Width = 252
+          end>
+      end
+      inherited BottomPanel: TPanel
+        Top = 207
+        Width = 256
+        inherited tbxValue: TEdit
+          Width = 110
+        end
+        inherited btnAll: TButton
+          Left = 186
+        end
+      end
+    end
+    inline fmxAccessMask: TBitsFrame
+      Left = 0
+      Top = 0
+      Width = 338
+      Height = 235
+      Align = alClient
+      Constraints.MinHeight = 100
+      Constraints.MinWidth = 200
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 0
+      inherited Tree: TDevirtualizedTree
+        Width = 338
+        Height = 207
+        Columns = <
+          item
+            Options = [coAllowClick, coDraggable, coEnabled, coParentBidiMode, coParentColor, coResizable, coVisible, coAutoSpring, coSmartResize, coAllowFocus, coDisableAnimatedResize, coEditable, coStyleColor]
+            Position = 0
+            Text = 'Name'
+            Width = 334
+          end>
+      end
+      inherited BottomPanel: TPanel
+        Top = 207
+        Width = 338
+        inherited tbxValue: TEdit
+          Width = 192
+        end
+        inherited btnAll: TButton
+          Left = 268
+        end
+      end
+    end
+  end
+  inline fmxCondition: TAceConditionFrame
+    Left = 0
+    Top = 154
+    Width = 600
+    Height = 25
+    Anchors = [akLeft, akTop, akRight]
+    Constraints.MinHeight = 25
+    Constraints.MinWidth = 200
+    Enabled = False
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 3
+  end
+  inline fmxExtraData: THexEditFrame
+    Left = 0
+    Top = 556
+    Width = 600
+    Height = 25
+    Anchors = [akLeft, akRight, akBottom]
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 7
+  end
+  inline fmxObjectType: TGuidFrame
+    Left = 0
+    Top = 464
+    Width = 600
+    Height = 21
+    Anchors = [akLeft, akRight, akBottom]
+    Constraints.MinWidth = 224
+    Enabled = False
+    TabOrder = 8
+  end
+  inline fmxInheritedObjectType: TGuidFrame
+    Left = 0
+    Top = 512
+    Width = 600
+    Height = 21
+    Anchors = [akLeft, akRight, akBottom]
+    Constraints.MinWidth = 224
+    Enabled = False
+    TabOrder = 9
+  end
+end
---
+++ Prototypes/NtUiFrame.Ace.pas
@@ -0,0 +1,255 @@
+unit NtUiFrame.Ace;
+
+{
+  This unit provides a frame for detailed editing of Access Control Entries.
+}
+
+interface
+
+uses
+  Winapi.Windows, System.SysUtils, System.Classes, Vcl.Graphics, Vcl.Controls,
+  Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Ntapi.WinNt, NtUtils.Security.Acl,
+  NtUiFrame.Bits, UI.Prototypes.Sid.Edit, Vcl.ExtCtrls, NtUiFrame.Ace.Condition,
+  NtUiFrame, NtUiFrame.Hex.Edit, NtUiFrame.Guid, NtUiCommon.Interfaces;
+
+type
+  TAceFrame = class(TFrame, ICanConsumeEscape)
+    cbxType: TComboBox;
+    lblType: TLabel;
+    lblFlags: TLabel;
+    lblAccessMask: TLabel;
+    lblSid: TLabel;
+    fmxSid: TSidEditor;
+    lblServerSid: TLabel;
+    fmxServerSid: TSidEditor;
+    cbxObjectType: TCheckBox;
+    fmxFlags: TBitsFrame;
+    fmxAccessMask: TBitsFrame;
+    cbxInheritedObjectType: TCheckBox;
+    BitsPanel: TPanel;
+    Splitter: TSplitter;
+    lblCondition: TLabel;
+    fmxCondition: TAceConditionFrame;
+    fmxExtraData: THexEditFrame;
+    lblExtraData: TLabel;
+    fmxObjectType: TGuidFrame;
+    fmxInheritedObjectType: TGuidFrame;
+    procedure cbxObjectTypeClick(Sender: TObject);
+    procedure cbxInheritedObjectTypeClick(Sender: TObject);
+    procedure cbxTypeChange(Sender: TObject);
+    procedure SplitterMoved(Sender: TObject);
+  private
+    FObjectFlags: TObjectAceFlags;
+    FMaskType: Pointer;
+    FGenericMapping: TGenericMapping;
+    FPreviousAceType: TAceType;
+    procedure SetAce(const Value: TAceData);
+    function GetAce: TAceData;
+    function GetAceType: TAceType;
+    procedure UpdateMaskType;
+  protected
+    procedure Loaded; override;
+    function ConsumesEscape: Boolean;
+  public
+    procedure LoadType(
+      AccessMaskType: Pointer;
+      const GenericMapping: TGenericMapping
+    );
+    property Ace: TAceData read GetAce write SetAce;
+  end;
+
+implementation
+
+uses
+  NtUtils, NtUtils.Security, UI.Colors, NtUiLib.TaskDialog;
+
+{$R *.dfm}
+
+{ TAceFrame }
+
+procedure TAceFrame.cbxInheritedObjectTypeClick;
+begin
+  fmxInheritedObjectType.Enabled := cbxInheritedObjectType.Checked;
+end;
+
+procedure TAceFrame.cbxObjectTypeClick;
+begin
+  fmxObjectType.Enabled := cbxObjectType.Checked;
+end;
+
+procedure TAceFrame.cbxTypeChange;
+var
+  AceType: TAceType;
+begin
+  AceType := GetAceType;
+  lblServerSid.Enabled := AceType = ACCESS_ALLOWED_COMPOUND_ACE_TYPE;
+  fmxServerSid.Enabled := AceType = ACCESS_ALLOWED_COMPOUND_ACE_TYPE;
+  lblCondition.Enabled := AceType in CallbackAces;
+  fmxCondition.Enabled := AceType in CallbackAces;
+  cbxObjectType.Enabled := AceType in ObjectAces;
+  fmxObjectType.Enabled := (AceType in ObjectAces) and cbxObjectType.Checked;
+  cbxInheritedObjectType.Enabled := AceType in ObjectAces;
+  fmxInheritedObjectType.Enabled := (AceType in ObjectAces) and
+    cbxInheritedObjectType.Checked;
+  lblExtraData.Enabled := not (AceType in CallbackAces);
+  fmxExtraData.Enabled := not (AceType in CallbackAces);
+
+  if (AceType = SYSTEM_MANDATORY_LABEL_ACE_TYPE) xor
+    (FPreviousAceType = SYSTEM_MANDATORY_LABEL_ACE_TYPE) then
+    UpdateMaskType;
+
+  FPreviousAceType := AceType;
+end;
+
+function TAceFrame.ConsumesEscape;
+begin
+  Result := cbxType.DroppedDown;
+end;
+
+function TAceFrame.GetAce;
+begin
+  Result := Default(TAceData);
+  Result.AceType := GetAceType;
+  Result.AceFlags := TAceFlags(fmxFlags.Value);
+  Result.Mask := TAccessMask(fmxAccessMask.Value);
+  Result.SID := fmxSid.Sid;
+
+  // Server SID
+  if Result.AceType = ACCESS_ALLOWED_COMPOUND_ACE_TYPE then
+  begin
+    Result.CompoundAceType := COMPOUND_ACE_IMPERSONATION;
+    Result.ServerSID := fmxServerSid.Sid;
+  end;
+
+  // Extra & callback data
+  if Result.AceType in CallbackAces then
+    Result.ExtraData := fmxCondition.Condition
+  else
+    Result.ExtraData := fmxExtraData.Data;
+
+  // Object & inherited object types
+  if Result.AceType in ObjectAces then
+  begin
+    Result.ObjectFlags := FObjectFlags and not
+      (ACE_OBJECT_TYPE_PRESENT or ACE_INHERITED_OBJECT_TYPE_PRESENT);
+
+    if cbxObjectType.Checked then
+    begin
+      Result.ObjectFlags := Result.ObjectFlags or ACE_OBJECT_TYPE_PRESENT;
+      Result.ObjectType := fmxObjectType.Guid;
+    end;
+
+    if cbxInheritedObjectType.Checked then
+    begin
+      Result.ObjectFlags := Result.ObjectFlags or
+        ACE_INHERITED_OBJECT_TYPE_PRESENT;
+      Result.InheritedObjectType := fmxInheritedObjectType.Guid;
+    end;
+  end;
+end;
+
+function TAceFrame.GetAceType;
+begin
+  if (cbxType.ItemIndex < 0) or (cbxType.ItemIndex > Integer(High(TAceType))) then
+    raise Exception.Create('Invalid ACE type.');
+
+  Result := TAceType(cbxType.ItemIndex);
+end;
+
+procedure TAceFrame.Loaded;
+begin
+  inherited;
+  fmxFlags.LoadType(TypeInfo(TAceFlags));
+  FMaskType := TypeInfo(TAccessMask);
+  UpdateMaskType;
+end;
+
+procedure TAceFrame.LoadType;
+begin
+  Assert(Assigned(AccessMaskType));
+  FMaskType := AccessMaskType;
+  FGenericMapping := GenericMapping;
+  UpdateMaskType;
+end;
+
+procedure TAceFrame.SetAce;
+const
+  UNRECOGNIZED_FLAGS_CAPTION = 'ACE Editor';
+  UNRECOGNIZED_FLAGS_INSTRUCTION = 'Unrecognized Object Flags';
+  UNRECOGNIZED_FLAGS_PROMPT = 'The ACE contains unrecognized object flags. ' +
+    'Do you want to preserve them during editing?';
+begin
+  // Validate the ACE type
+  if Value.AceType > High(TAceType) then
+    raise Exception.Create('Unknown ACE type.');
+
+  if (Value.AceType = ACCESS_ALLOWED_COMPOUND_ACE_TYPE) and
+    (Value.CompoundAceType <> COMPOUND_ACE_IMPERSONATION) then
+    raise Exception.Create('Unknown compound ACE sub-type');
+
+  // Save unrecognized flags
+  FObjectFlags := 0;
+  if (Value.AceType in ObjectAces) and HasAny(Value.ObjectFlags and not
+    (ACE_OBJECT_TYPE_PRESENT or ACE_INHERITED_OBJECT_TYPE_PRESENT)) and
+    (UsrxShowTaskDialog(Handle, UNRECOGNIZED_FLAGS_CAPTION,
+    UNRECOGNIZED_FLAGS_INSTRUCTION, UNRECOGNIZED_FLAGS_PROMPT, diInfo, dbYesNo)
+    = IDYES) then
+    FObjectFlags := Value.ObjectFlags;
+
+  // Update component state
+  cbxType.ItemIndex := Integer(Value.AceType);
+  fmxSid.Sid := Value.SID;
+  fmxFlags.Value := Value.AceFlags;
+  fmxAccessMask.Value := Value.Mask;
+
+  if Value.AceType = ACCESS_ALLOWED_COMPOUND_ACE_TYPE then
+    fmxServerSid.Sid := Value.ServerSID
+  else
+    fmxServerSid.Sid := nil;
+
+  if Value.AceType in ObjectAces then
+  begin
+    cbxObjectType.Checked := BitTest(Value.ObjectFlags and
+      ACE_OBJECT_TYPE_PRESENT);
+    cbxInheritedObjectType.Checked := BitTest(Value.ObjectFlags and
+      ACE_INHERITED_OBJECT_TYPE_PRESENT);
+    fmxObjectType.Guid := Value.ObjectType;
+    fmxInheritedObjectType.Guid := Value.InheritedObjectType;
+  end
+  else
+  begin
+    cbxObjectType.Checked := False;
+    cbxInheritedObjectType.Checked := False;
+    fmxObjectType.Guid := Default(TGuid);
+    fmxInheritedObjectType.Guid := Default(TGuid);
+  end;
+
+  if Value.AceType in CallbackAces then
+  begin
+    fmxCondition.TrySetCondition(Value.ExtraData);
+    fmxExtraData.Data := nil;
+  end
+  else
+  begin
+    fmxCondition.TrySetCondition(nil);
+    fmxExtraData.Data := Value.ExtraData;
+  end;
+
+  // Update enabled state
+  cbxTypeChange(Self);
+end;
+
+procedure TAceFrame.SplitterMoved;
+begin
+  lblFlags.Left := fmxFlags.Left;
+end;
+
+procedure TAceFrame.UpdateMaskType;
+begin
+  if GetAceType = SYSTEM_MANDATORY_LABEL_ACE_TYPE then
+    fmxAccessMask.LoadType(TypeInfo(TMandatoryLabelMask))
+  else
+    fmxAccessMask.LoadAccessMaskType(FMaskType, FGenericMapping, True, False);
+end;
+
+end.
